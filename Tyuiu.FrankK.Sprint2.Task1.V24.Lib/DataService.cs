31b7ab473a661dc934b70196c2437ef3faf5f5b1
PashkovGrigoriy <gripas2006@gmail.com>
2025-10-27 03:48:29 +0500

    Tsak1

---
+++ Tyuiu.FrankK.Sprint2.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FrankK.Sprint2.Task1.V24.Lib
+{
+    public class DataService : ISprint2Task1V24
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            //a=325, b=325, c=242, d=324
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (c < d);
+            res[1] = (a <= b) & ((c + (323 - 242)) == d);
+            res[2] = (a > b) || (c < d);
+            res[3] = (a < b) && ((c + (541 - 242)) != d);
+            res[4] = !(c > d);
+            res[5] = (a == b) ^ (c != d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FrankK.Sprint2.Task1.V24/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.FrankK.Sprint2.Task1.V24.Lib;
+namespace Tyuiu.FrankK.Sprint2.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 325;
+            int b = 325;
+            int c = 242;
+            int d = 324;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
