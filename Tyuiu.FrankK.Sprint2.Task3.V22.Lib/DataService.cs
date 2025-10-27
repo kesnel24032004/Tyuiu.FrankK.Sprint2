5f9e53ac8fd0ee7094a08c37f1d0bd138c6f8c3f
PashkovGrigoriy <gripas2006@gmail.com>
2025-10-27 11:30:59 +0500

    Task3

--- Tyuiu.FrankK.Sprint2.Task3.V22.Lib/DataService.cs
+++ Tyuiu.FrankK.Sprint2.Task3.V22.Lib/DataService.cs
@@ -6,13 +6,13 @@ namespace Tyuiu.FrankK.Sprint2.Task3.V22.Lib
         public double Calculate(double x)
         {
             if (x > 1)
-                return x + Math.Pow(((x + 3) / (x - 1)), x);
+                return Math.Round(x + Math.Pow(((x + 3) / (x - 1)), x),3);
             else if (x == 0)
-                return (Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) + 10) / (Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12);
+                return Math.Round((Math.Pow(x, 2) - Math.Cos(Math.Pow(x, 2)) + 10) / (Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12),3);
             else if ((x > (-26)) & (x < 2))
-                return Math.Pow((3 + (2 / Math.Pow(x, 2))), x);
+                return Math.Round(Math.Pow((3 + (2 / Math.Pow(x, 2))), x), 3);
             else 
-                return (x + (10 * x) - (1 / x));
+                return Math.Round((x + (10 * x) - (1 / x)), 3);
         }
     }
 }
--- Tyuiu.FrankK.Sprint2.Task3.V22/Program.cs
+++ Tyuiu.FrankK.Sprint2.Task3.V22/Program.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.FrankK.Sprint2.Task3.V22
             Console.WriteLine("Введите Х: ");
             double x;
             x = Convert.ToDouble(Console.ReadLine());
-            double res = Math.Round(ds.Calculate(x),3);
+            double res = ds.Calculate(x);
             Console.WriteLine(res);
             Console.ReadKey();
         }
